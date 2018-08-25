7602f7e079820aff58a31d3e56b84b37efcf43de
Jiren Jin <jinjiren@foxmail.com>
2018-08-25 15:29:49 +0900

    solve parity problem

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,8 +2,14 @@ from test_framework import generic_test
 
 
 def parity(x):
-    # TODO - you fill in here.
-    return 0
+    length = 64
+    while length > 1:
+        # note that we need to use `//` here, otherwise length will be `float`,
+        # and you get "unsupported operand type(s) for >>: 'int' and 'float'"
+        # I believe this is specific to python3
+        length //= 2
+        x ^= x >> length
+    return x & 1
 
 
 if __name__ == '__main__':
